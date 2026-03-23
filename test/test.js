742808f5d44479761b684cf2951f6f2ffb60682e
Josan866 <joshua.millner@online.de>
2026-03-23 16:05:16 +0100

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ const urlParams = new URLSearchParams(urlpar)
 let urlpar1;
 
 urlpar1 = urlParams.get('name')
-document.getElementById('name').innerhtml = urlpar1;
+document.getElementById('name').innerHTML = urlpar1;
 if (urlpar1 == "Nick") {
     
 }
 urlpar1 = urlParams.get('time')
-document.getElementById('time').innerhtml = urlpar1;
+document.getElementById('time').innerHTML = urlpar1;
